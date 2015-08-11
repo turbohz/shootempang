23f0e19304f37a8aa2885618168a53149f8cc051
Gerard Ruiz <gerard@mailtrack.io>
2015-08-11 19:07:18 +0200

    Adds crude player movement

--- desktop/src-common/pang/core.clj
+++ desktop/src-common/pang/core.clj
@@ -3,17 +3,30 @@
             [play-clj.ui :refer :all]
             [play-clj.g2d :refer :all]))
 
+(defn add-to [e k v] (assoc e k (+ (k e) v)))
+
+(defn map-if [c f xs]
+  (->> xs (map (fn [x]
+                 (if (c x) (f x) x)
+                 )))
+  )
+
+(defn player? [e] (= :player (:type e)))
+(def valid-controls #{(key-code :dpad-left) (key-code :dpad-right)})
+
 (defscreen main-screen
+
            :on-show
            (fn [screen entities]
              (update! screen :renderer (stage))
              (let [
                    sheet (texture "tiles.png")
                    tiles (texture! sheet :split 32 32)
+
                    player (texture (aget tiles 0 0))
-                   player (assoc player :x 0 :y 0 :width 32 :height 32)
+                   player (assoc player :x 0 :y 0 :width 32 :height 32 :type :player)
                    meteor (texture (aget tiles 1 0))
-                   meteor (assoc meteor :x 300 :y 300 :width 32 :height 32)
+                   meteor (assoc meteor :x 300 :y 300 :width 32 :height 32 :type :meteor)
                    ]
                [player meteor]
                )
@@ -25,7 +38,22 @@
              (render! screen entities)
              )
 
-           )
+           :on-key-down
+           (fn [screen entities]
+             (when
+               (contains? valid-controls (:key screen))
+               (let [
+                     key (:key screen)
+                     move (fn [player] (cond
+                                         (= key (key-code :dpad-right))
+                                         (add-to player :x 8)
+                                         (= key (key-code :dpad-left))
+                                         (add-to player :x -8)))
+                     entities (->> entities (map-if player? move))
+                     ]
+
+                 entities)
+               )))
 
 (defgame pang-game
          :on-create
