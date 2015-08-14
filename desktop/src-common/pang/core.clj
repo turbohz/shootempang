245800cb0761a44569be7e7e6146a61a55649760
Gerard Ruiz <gerard@mailtrack.io>
2015-08-14 21:00:04 +0200

    Implements wall collision

--- desktop/src-common/pang/core.clj
+++ desktop/src-common/pang/core.clj
@@ -18,10 +18,25 @@
   (let [
         position (vector-2 (:x e) (:y e))
         position (vector-2! position :add (:velocity e))]
-    (-> e (assoc-in [:x] (.x position)) (assoc-in [:y] (.y position)))))
+    (-> e (assoc-in [:x] (x position)) (assoc-in [:y] (y position)))))
+
+(defn reflect-vector-component [c v]
+    (case c
+      :x (x! v (* -1 (x v)))
+      :y (y! v (* -1 (y v)))
+      v
+      )
+  )
+
+(defn enforce-bounds [e screen]
+    (cond
+      (or (< (:x e) 0) (> (:x e) (width screen))) (assoc-in e [:velocity] (reflect-vector-component :x (:velocity e)))
+      (or (< (:y e) 0) (> (:y e) (height screen))) (assoc-in e [:velocity] (reflect-vector-component :y (:velocity e)))
+      :else e)
+  )
 
 (defn animate [screen entities]
-  (map (fn [e] (-> e update-velocity update-position))
+  (map (fn [e] (-> e update-velocity update-position (enforce-bounds screen)))
        entities))
 
 (defscreen main-screen
