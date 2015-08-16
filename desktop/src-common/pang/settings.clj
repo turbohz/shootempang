21acf1bd78161986d2420bdde8579312865377d9
Gerard Ruiz <gerard@mailtrack.io>
2015-08-16 17:36:00 +0200

    Creates settings file

---
+++ desktop/src-common/pang/settings.clj
@@ -0,0 +1,12 @@
+(ns pang.settings)
+(def settings
+  {
+   :gravity -0.1
+   :player  {
+             :x-speed 4}
+   :enemy   {
+             :x-speed 2
+             :y-speed 8
+             }
+
+   })
